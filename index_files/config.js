357ff28192ae656c0a98b7820bcdd305b88b6f82
namefanwjcom <32805485+namefanwjcom@users.noreply.github.com>
2019-12-18 19:04:17 +0800

    Add files via upload

---
+++ index_files/config.js
@@ -0,0 +1,8 @@
+/* Start/end date of semester. Make sure to change utils.py as well */
+var startDate = '2019-06-24', endDate = '2019-08-16';
+
+/* Calendar Specification */
+var officeHoursCalendarId = 'berkeley.edu_j61jfug4uec0qmua3p9jfmer3g@group.calendar.google.com';
+var instructorOfficeHoursCalendarId = 'berkeley.edu_h1hsei6cabfgs6mnll950mr2h4@group.calendar.google.com';
+var lecturesCalendarId = 'berkeley.edu_eeojplpkjpc38ubt556n3su9os@group.calendar.google.com';
+var sectionsCalendarId = 'berkeley.edu_7l6kk1q31rdu37banpio4snl2k@group.calendar.google.com';
